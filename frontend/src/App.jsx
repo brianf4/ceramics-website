97ef1711cb2615520a6458f3727fa80f15ea200c
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-06-09 22:17:12 -0700

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <Navbar />
+          {/* Page content here */}
+          <h1>All content goes here?</h1>
+          <section className="border-2 border-black">hello</section>
+
+
+          
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
