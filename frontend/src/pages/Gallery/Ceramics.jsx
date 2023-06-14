3a113eba8bb4055dcb66fe91570884ca0db74ddb
Brian Fuentes <Brian.Fuentes04@gmail.com>
2023-06-13 18:58:35 -0700

    working on picture sizing

--- frontend/src/pages/Gallery/Ceramics.jsx
+++ frontend/src/pages/Gallery/Ceramics.jsx
@@ -1,115 +1,134 @@
-import React, { useState } from "react";
+import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 function Ceramics() {
-  const [imageLoaded, setImageLoaded] = useState(false);
 
 
   return (
-    <figure className="border-2 border-purple-700 flex flex-wrap">
-      <LazyLoadImage
-        className="w-max"
-        alt="ceramic picture 1"
-        src="../images/IMG_9318.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9322.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9336.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9343.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9352.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9354.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9362.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9367.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9371.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9318.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9376.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9394.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9414.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9418.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9439.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9441.jpeg"
-        effect="blur"
-      />
-      <LazyLoadImage
-        
-        alt="ceramic picture 1"
-        src="../images/IMG_9446.jpeg"
-        effect="blur"
-      />
+    <figure className="border-2 border-purple-700 flex flex-wrap gap-4">
+      <div className="w-1/6">
+        <LazyLoadImage
+          className=""
+          alt="ceramic picture 1"
+          src="../images/IMG_9318.jpeg"
+          effect="blur"
+          />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          className=""
+          alt="ceramic picture 1"
+          src="../images/IMG_9322.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          className=""
+          alt="ceramic picture 1"
+          src="../images/IMG_9336.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9343.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9352.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9354.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9362.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9367.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9371.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9318.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9376.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9394.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9414.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9418.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9439.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9441.jpeg"
+          effect="blur"
+        />
+      </div>
+      <div className="w-1/6">
+        <LazyLoadImage
+          alt="ceramic picture 1"
+          src="../images/IMG_9446.jpeg"
+          effect="blur"
+        />
+      </div>
     </figure>
   );
 }
